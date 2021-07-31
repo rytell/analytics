0fa05da7e1975130ea543b1a6c2862045cf9c115
José Marín <jose.marin1997@gmail.com>
2021-07-31 14:40:02 -0400

    style: updates primary color

--- src/pages/GlobalPage.js
+++ src/pages/GlobalPage.js
@@ -65,7 +65,7 @@ function GlobalPage() {
 
   return (
     <PageWrapper>
-      <ThemedBackground backgroundColor={transparentize(0.7, '#FF6b00')} />
+      <ThemedBackground backgroundColor={transparentize(0.7, '#3B0084')} />
       <ContentWrapper>
         <div>
           <AutoColumn gap="24px" style={{ paddingBottom: below800 ? '0' : '24px' }}>
